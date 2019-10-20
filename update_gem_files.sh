b801a74045fa955f0538e5f10d8a2868e08f18bd
Leif Ringstad <leif@symbolia.com>
2019-10-20 23:46:57 +0200

    Update gems

--- update_gem_files.sh
+++ update_gem_files.sh
@@ -6,4 +6,5 @@ cp -f Gemfile* ./dev-no-entrypoint/
 cp -f Gemfile* ./dev-slim-no-entrypoint/
 cp -f Gemfile* ./alpine/
 cp -f Gemfile* ./slim/
+cp -f Gemfile* ./mysql8-client/
 # gc -m "Update gems"
