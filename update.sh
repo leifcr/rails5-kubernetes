ef269afc198df3eb32f32464b8e8c187a26264d6
Leif Ringstad <leifcr@gmail.com>
2021-08-02 12:51:41 +0200

    Improve update script

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+bundle update
 cp -f Gemfile* ./dev/
 cp -f Gemfile* ./dev-alpine/
 cp -f Gemfile* ./dev-slim/
@@ -8,4 +9,11 @@ cp -f Gemfile* ./alpine/
 cp -f Gemfile* ./slim/
 cp -f Gemfile* ./mysql8-client/
 cp -f Gemfile* ./dev-mysql8/
+cd dev
+yarn upgrade
+cp yarn.lock ../dev-no-entrypoint/
+cp yarn.lock ../dev-mysql8/
+yarn outdated
+cd ..
+bundle outdated --strict
 # gc -m "Update gems"
